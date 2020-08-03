777a942db108a885b788d13db44fa911a9767b93
Tatyana <tatyana@Tatyanas-MacBook-Pro.local>
2020-08-03 20:52:06 +0300

    Use env vars in config

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-user = 'user'
-password = 'password'
-database = 'database'
-# host = '127.0.0.1'
-host = 'host.docker.internal'
-port = 5432
+import os
+
+user = os.environ.get('DB_USER', 'user')
+password = os.environ.get('DB_PASSWORD', 'password')
+database = os.environ.get('DB_NAME', 'database')
+host = os.environ.get('DB_HOST', '127.0.0.1')
+port = int(os.environ.get('DB_PORT', 5432))
--- start_service.sh
+++ start_service.sh
@@ -1,5 +1,6 @@
 docker build -t service .
 docker run -it --rm \
     --name service \
+    -e DB_HOST="host.docker.internal" \
     -p 1234:1234 \
     service
